fd6a01826e1f65795e3b220bdbbd6f9696a71f0d
stark-eth <1319114370@qq.com>
2023-03-06 01:28:36 +0800

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,33 @@
-import { defineConfig } from 'astro/config'
-import unocss from 'unocss/astro'
-import { presetUno } from 'unocss'
-import presetAttributify from '@unocss/preset-attributify'
-import presetTypography from '@unocss/preset-typography'
-import solidJs from '@astrojs/solid-js'
-import reactJs from '@astrojs/react-js' // 添加这一行
-import vercelDisableBlocks from './plugins/vercelDisableBlocks'
-
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/edge'
-
+import { defineConfig } from 'astro/config';
+import unocss from 'unocss/astro';
+import { presetUno } from 'unocss';
+import presetAttributify from '@unocss/preset-attributify';
+import presetTypography from '@unocss/preset-typography';
+import solidJs from '@astrojs/solid-js';
+import vercelDisableBlocks from './plugins/vercelDisableBlocks';
+import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/edge';
+import react from "@astrojs/react";
 const envAdapter = () => {
   if (process.env.OUTPUT == 'vercel') {
-    return vercel()
+    return vercel();
   } else {
     return node({
       mode: 'standalone'
-    })
+    });
   }
-}
+};
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss({
-      presets: [
-        presetAttributify(),
-        presetUno(),
-        presetTypography(),
-      ]
-    }),
-    solidJs(),
-    reactJs() // 添加这一行
-  ],
+  integrations: [unocss({
+    presets: [presetAttributify(), presetUno(), presetTypography()]
+  }), solidJs(), react()],
   output: 'server',
   adapter: envAdapter(),
   vite: {
-    plugins: [
-      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
-    ]
-  },
+    plugins: [process.env.OUTPUT == 'vercel' && vercelDisableBlocks()]
+  }
 });
