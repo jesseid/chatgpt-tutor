56f380ffc49172fdd2010d507ba2dd461241dd90
stark <songgao.cao@outlook.com>
2024-03-19 11:56:04 +0400

    fix contract

--- contract/contracts/DBR.sol
+++ contract/contracts/DBR.sol
@@ -1,188 +1,224 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155.sol';
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155Metadata.sol';
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155MintBurn.sol';
+import "./Strings.sol";
 
-contract DBR is ERC721Enumerable, Ownable {
-    using SafeMath for uint256;
-    uint256 public DBRPrice = 50000000000000000; // 0.05 ETH
-    uint256 public maxDBRPurchase = 20;
-    uint256 public maxDBRs = 10000;
-    uint256 public _reserved = 20;
-    string public _baseTokenURI;
-    address admin;
-    address[] public adminList;
-    mapping(address => bool) public admins;
+contract OwnableDelegateProxy { }
 
-    bool public saleIsActive = false;
-    bool public reserved = false;
-    bool public allFrozen;
-    mapping(uint256 => bool) frozenIds;
-    // Optional mapping for token URIs
-    mapping(uint256 => string) private _tokenURIs;
-    event DBRPriceChanged(uint256 price);
-    event MaxTokenAmountChanged(uint256 value);
-    event MaxPurchaseChanged(uint256 value);
-    event DBRsReserved();
-    event RolledOver(bool status);
-    event PermanentURI(string _value, uint256 indexed _id);
+contract ProxyRegistry {
+  mapping(address => OwnableDelegateProxy) public proxies;
+}
 
-    modifier onReserve() {
-        require(!reserved, "Tokens reserved");
-        _;
-        reserved = true;
-        emit DBRsReserved();
+/**
+ * @title ERC1155Tradable
+ * ERC1155Tradable - ERC1155 contract that whitelists an operator address, has create and mint functionality, and supports useful standards from OpenZeppelin,
+  like _exists(), name(), symbol(), and totalSupply()
+ */
+contract ERC1155Tradable is ERC1155, ERC1155MintBurn, ERC1155Metadata, Ownable {
+  using Strings for string;
+
+  address proxyRegistryAddress;
+  uint256 private _currentTokenID = 0;
+  mapping (uint256 => address) public creators;
+  mapping (uint256 => uint256) public tokenSupply;
+  // Contract name
+  string public name;
+  // Contract symbol
+  string public symbol;
+
+  /**
+   * @dev Require msg.sender to be the creator of the token id
+   */
+  modifier creatorOnly(uint256 _id) {
+    require(creators[_id] == msg.sender, "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
+    _;
+  }
+
+  /**
+   * @dev Require msg.sender to own more than 0 of the token id
+   */
+  modifier ownersOnly(uint256 _id) {
+    require(balances[msg.sender][_id] > 0, "ERC1155Tradable#ownersOnly: ONLY_OWNERS_ALLOWED");
+    _;
+  }
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    address _proxyRegistryAddress
+  ) public {
+    name = _name;
+    symbol = _symbol;
+    proxyRegistryAddress = _proxyRegistryAddress;
+  }
+
+  function uri(
+    uint256 _id
+  ) public view returns (string memory) {
+    require(_exists(_id), "ERC721Tradable#uri: NONEXISTENT_TOKEN");
+    return Strings.strConcat(
+      baseMetadataURI,
+      Strings.uint2str(_id)
+    );
+  }
+
+  /**
+    * @dev Returns the total quantity for a token ID
+    * @param _id uint256 ID of the token to query
+    * @return amount of token in existence
+    */
+  function totalSupply(
+    uint256 _id
+  ) public view returns (uint256) {
+    return tokenSupply[_id];
+  }
+
+  /**
+   * @dev Will update the base URL of token's URI
+   * @param _newBaseMetadataURI New base URL of token's URI
+   */
+  function setBaseMetadataURI(
+    string memory _newBaseMetadataURI
+  ) public onlyOwner {
+    _setBaseMetadataURI(_newBaseMetadataURI);
+  }
+
+  /**
+    * @dev Creates a new token type and assigns _initialSupply to an address
+    * NOTE: remove onlyOwner if you want third parties to create new tokens on your contract (which may change your IDs)
+    * @param _initialOwner address of the first owner of the token
+    * @param _initialSupply amount to supply the first owner
+    * @param _uri Optional URI for this token type
+    * @param _data Data to pass if receiver is contract
+    * @return The newly created token ID
+    */
+  function create(
+    address _initialOwner,
+    uint256 _initialSupply,
+    string calldata _uri,
+    bytes calldata _data
+  ) external onlyOwner returns (uint256) {
+
+    uint256 _id = _getNextTokenID();
+    _incrementTokenTypeId();
+    creators[_id] = msg.sender;
+
+    if (bytes(_uri).length > 0) {
+      emit URI(_uri, _id);
     }
 
-    modifier onlyAdmin() {
-        require(admins[msg.sender], "Not admin");
-        _;
+    _mint(_initialOwner, _id, _initialSupply, _data);
+    tokenSupply[_id] = _initialSupply;
+    return _id;
+  }
+
+  /**
+    * @dev Mints some amount of tokens to an address
+    * @param _to          Address of the future owner of the token
+    * @param _id          Token ID to mint
+    * @param _quantity    Amount of tokens to mint
+    * @param _data        Data to pass if receiver is contract
+    */
+  function mint(
+    address _to,
+    uint256 _id,
+    uint256 _quantity,
+    bytes memory _data
+  ) public creatorOnly(_id) {
+    _mint(_to, _id, _quantity, _data);
+    tokenSupply[_id] = tokenSupply[_id].add(_quantity);
+  }
+
+  /**
+    * @dev Mint tokens for each id in _ids
+    * @param _to          The address to mint tokens to
+    * @param _ids         Array of ids to mint
+    * @param _quantities  Array of amounts of tokens to mint per id
+    * @param _data        Data to pass if receiver is contract
+    */
+  function batchMint(
+    address _to,
+    uint256[] memory _ids,
+    uint256[] memory _quantities,
+    bytes memory _data
+  ) public {
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 _id = _ids[i];
+      require(creators[_id] == msg.sender, "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
+      uint256 quantity = _quantities[i];
+      tokenSupply[_id] = tokenSupply[_id].add(quantity);
+    }
+    _batchMint(_to, _ids, _quantities, _data);
+  }
+
+  /**
+    * @dev Change the creator address for given tokens
+    * @param _to   Address of the new creator
+    * @param _ids  Array of Token IDs to change creator
+    */
+  function setCreator(
+    address _to,
+    uint256[] memory _ids
+  ) public {
+    require(_to != address(0), "ERC1155Tradable#setCreator: INVALID_ADDRESS.");
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 id = _ids[i];
+      _setCreator(_to, id);
+    }
+  }
+
+  /**
+   * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-free listings.
+   */
+  function isApprovedForAll(
+    address _owner,
+    address _operator
+  ) public view returns (bool isOperator) {
+    // Whitelist OpenSea proxy contract for easy trading.
+    ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+    if (address(proxyRegistry.proxies(_owner)) == _operator) {
+      return true;
     }
 
-    constructor(address[] memory _admin) ERC721("DontBuyRocks", "DBR") {
-        for (uint256 i = 0; i < _admin.length; i++) {
-            admins[_admin[i]] = true;
-        }
-        adminList = _admin;
-    }
+    return ERC1155.isApprovedForAll(_owner, _operator);
+  }
 
-    function withdraw() public onlyAdmin {
-        uint256 balance = address(this).balance;
-        uint256 _each = balance / adminList.length;
-        for (uint256 i = 0; i < adminList.length; i++) {
-            require(payable(adminList[i]).send(_each));
-        }
-    }
+  /**
+    * @dev Change the creator address for given token
+    * @param _to   Address of the new creator
+    * @param _id  Token IDs to change creator of
+    */
+  function _setCreator(address _to, uint256 _id) internal creatorOnly(_id)
+  {
+      creators[_id] = _to;
+  }
 
-    function reserveDBRs(address _to, uint256 _amount)
-        external
-        onlyAdmin
-        onReserve
-    {
-        require(_amount <= _reserved, "Exceeds reserved Cat supply");
+  /**
+    * @dev Returns whether the specified token exists by checking to see if it has a creator
+    * @param _id uint256 ID of the token to query the existence of
+    * @return bool whether the token exists
+    */
+  function _exists(
+    uint256 _id
+  ) internal view returns (bool) {
+    return creators[_id] != address(0);
+  }
 
-        uint256 supply = totalSupply();
-        for (uint256 i; i < _amount; i++) {
-            _safeMint(_to, supply + i);
-        }
+  /**
+    * @dev calculates the next token ID based on value of _currentTokenID
+    * @return uint256 for the next token ID
+    */
+  function _getNextTokenID() private view returns (uint256) {
+    return _currentTokenID.add(1);
+  }
 
-        _reserved -= _amount;
-    }
-
-    function flipSaleState() public onlyAdmin {
-        saleIsActive = !saleIsActive;
-        emit RolledOver(saleIsActive);
-    }
-
-    function mintDBRs(uint256 num) public payable {
-        uint256 supply = totalSupply();
-        require(saleIsActive, "Sale is not active");
-        require(num > 0, "Cannot buy 0");
-        require(
-            num < maxDBRPurchase,
-            "Exceeds max number of DBRs in one transaction"
-        );
-        require(
-            supply + num < maxDBRs - _reserved,
-            "Purchase would exceed max supply of DBRs"
-        );
-        require(
-            DBRPrice.mul(num) <= msg.value,
-            "Ether value sent is not correct"
-        );
-        uint256 mintIndex;
-        for (uint256 i; i < num; i++) {
-            mintIndex = supply + i;
-            _safeMint(msg.sender, supply + i);
-        }
-    }
-
-    function walletOfOwner(address _owner)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256 tokenCount = balanceOf(_owner);
-
-        uint256[] memory tokensId = new uint256[](tokenCount);
-        for (uint256 i; i < tokenCount; i++) {
-            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
-        }
-        return tokensId;
-    }
-
-    function getMyAssets(address _owner, uint256 index)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 tokensId = tokenOfOwnerByIndex(_owner, index);
-        return tokensId;
-    }
-
-    function setTokenURI(uint256 tokenId, string memory _tokenURI)
-        external
-        onlyAdmin
-    {
-        require(!allFrozen && !frozenIds[tokenId], "Already frozen");
-        _tokenURIs[tokenId] = _tokenURI;
-    }
-
-    function setBaseTokenURI(string memory baseTokenURI_) external onlyAdmin {
-        require(!allFrozen, "Already frozen");
-        _baseTokenURI = baseTokenURI_;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseTokenURI;
-    }
-
-    function setPrice(uint256 _price) external onlyAdmin {
-        require(_price > 0, "Zero price");
-
-        DBRPrice = _price;
-        emit DBRPriceChanged(_price);
-    }
-
-    function setMaxTokenAmount(uint256 _value) external onlyAdmin {
-        require(
-            _value > totalSupply() && _value <= 10_000,
-            "Wrong value for max supply"
-        );
-
-        maxDBRs = _value;
-        emit MaxTokenAmountChanged(_value);
-    }
-
-    function setMaxPurchase(uint256 _value) external onlyAdmin {
-        require(_value > 0, "Very low value");
-
-        maxDBRPurchase = _value;
-        emit MaxPurchaseChanged(_value);
-    }
-
-    function setReserveAmount(uint256 __reserved) external onlyAdmin {
-        _reserved = __reserved;
-    }
-
-    function enableAdmin(address _addr) external onlyOwner {
-        admins[_addr] = true;
-    }
-
-    function disableAdmin(address _addr) external onlyOwner {
-        admins[_addr] = false;
-    }
-
-    function freezeAll() external onlyOwner {
-        allFrozen = true;
-    }
-
-    function freeze(uint256 tokenId) external onlyOwner {
-        frozenIds[tokenId] = true;
-
-        emit PermanentURI(tokenURI(tokenId), tokenId);
-    }
+  /**
+    * @dev increments the value of _currentTokenID
+    */
+  function _incrementTokenTypeId() private  {
+    _currentTokenID++;
+  }
 }
